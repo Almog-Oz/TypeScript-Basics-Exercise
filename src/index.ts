533071de9874b0e3259a1f56f325c47b1a1f8a30
Almog <almogoz811@gmail.com>
2023-01-12 20:31:53 +0200

    upload typescript basics part1 - after CR

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const getAverage = (numsArray : number[]) : number => (
+    numsArray.reduce((sum, item) => sum + item, 0) / numsArray.length)
+
+const getAmountOfPositive = (numsArray : number[]) : number => (
+    numsArray.filter(value => value > 0).length)
+
+const sortList = (numsArray : number[]) => (
+    numsArray.sort((a, b) => (a - b)))
